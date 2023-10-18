f022e4cfefd1ead6dd3d40b7f2d47b9ba658e26c
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-10-18 21:29:52 +0600

    user delete crud operation with database finish properly

--- index.js
+++ index.js
@@ -40,6 +40,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/products/:id', async(req, res) =>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await productCollection.deleteOne(query);
+        res.send(result);
+    })
+
     // app.post('/products/:id', async(req, res) =>{
     //     const id = req.params.id;
     //     const query = {_id: new ObjectId(id)};
